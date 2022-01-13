fbc70d702079b573d23ca66532d09bc66a0052d2
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2022-01-13 20:00:47 +0200

    .

--- lib/widgets/homewidget.dart
+++ lib/widgets/homewidget.dart
@@ -48,42 +48,42 @@ class _HomeWidgetState extends State<HomeWidget> {
                   //isDarkMode: true,
                 );
               })
-          : new Center(
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.6,
-                child: new TinderSwapCard(
-                  swipeUp: true,
-                  swipeDown: true,
-                  orientation: AmassOrientation.BOTTOM,
-                  totalNum: welcomeImages.length,
-                  stackNum: 3,
-                  swipeEdge: 4.0,
-                  maxWidth: MediaQuery.of(context).size.width * 0.9,
-                  maxHeight: MediaQuery.of(context).size.width * 0.9,
-                  minWidth: MediaQuery.of(context).size.width * 0.8,
-                  minHeight: MediaQuery.of(context).size.width * 0.8,
-                  cardBuilder: (context, index) => Card(
-                    child:
-                        Image.network(api.baseURL + api.playingPosters[index]),
-                    //print(api.baseURL + api.playingPosters[index])
-                  ),
-                  cardController: controller = CardController(),
-                  swipeUpdateCallback:
-                      (DragUpdateDetails details, Alignment align) {
-                    /// Get swiping card's alignment
-                    if (align.x < 0) {
-                      //Card is LEFT swiping
-                    } else if (align.x > 0) {
-                      //Card is RIGHT swiping
-                    }
-                  },
-                  swipeCompleteCallback:
-                      (CardSwipeOrientation orientation, int index) {
-                    /// Get orientation & index of swiped card!
-                  },
+          : /*new Center(
+              child: */
+          Container(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: new TinderSwapCard(
+                //swipeUp: true,
+                //swipeDown: true,
+                orientation: AmassOrientation.BOTTOM,
+                totalNum: api.nowPlaying.length,
+                stackNum: 3,
+                swipeEdge: 4.0,
+                maxWidth: MediaQuery.of(context).size.width * 0.9,
+                maxHeight: MediaQuery.of(context).size.width * 0.9,
+                minWidth: MediaQuery.of(context).size.width * 0.8,
+                minHeight: MediaQuery.of(context).size.width * 0.8,
+                cardBuilder: (context, index) => Card(
+                  child: Image.network(api.baseURL + api.playingPosters[index]),
+                  //print(api.baseURL + api.playingPosters[index])
                 ),
+                cardController: controller = CardController(),
+                swipeUpdateCallback:
+                    (DragUpdateDetails details, Alignment align) {
+                  /// Get swiping card's alignment
+                  if (align.x < 0) {
+                    //Card is LEFT swiping
+                  } else if (align.x > 0) {
+                    //Card is RIGHT swiping
+                  }
+                },
+                swipeCompleteCallback:
+                    (CardSwipeOrientation orientation, int index) {
+                  /// Get orientation & index of swiped card!
+                },
               ),
             ),
+      // ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
