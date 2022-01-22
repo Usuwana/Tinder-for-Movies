b71d1fcbf6e4e6c18c32e64f48dedb3e6b5549d2
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2022-01-22 19:53:51 +0200

    .

--- lib/widgets/liked_movies.dart
+++ lib/widgets/liked_movies.dart
@@ -52,24 +52,68 @@ class _LikedMoviesState extends State<LikedMovies> {
             : ListView.builder(
                 itemCount: api.likedTitles.length,
                 itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            child: Image.network(
-                              api.baseURL + api.likedPosters[index],
+                  return Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Column(
+                      children: [
+                        SingleChildScrollView(
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(0.0),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 100,
+                                      width: 100,
+                                      child: Image.network(
+                                        api.baseURL + api.likedPosters[index],
+                                      ),
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text(
+                                          api.likedTitles[index].toString(),
+                                          style:
+                                              GoogleFonts.getFont('Montserrat')
+                                                  .copyWith(
+                                                      fontSize: 15,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: Colors.white),
+                                        ),
+                                        Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.5,
+                                          child: ReadMoreText(
+                                              api.likedOverviews[index]
+                                                  .toString(),
+                                              trimLines: 5,
+                                              colorClickableText: Colors.pink,
+                                              trimMode: TrimMode.Line,
+                                              trimCollapsedText: '...Show more',
+                                              trimExpandedText: ' show less',
+                                              style: GoogleFonts.getFont(
+                                                      'Montserrat')
+                                                  .copyWith(
+                                                fontSize: 11,
+                                                color: Colors.black,
+                                                /*backgroundColor: Colors.blueGrey*/
+                                              )),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
-                          Column(
-                            children: [
-                              Text(api.likedTitles[index].toString()),
-                              Text(api.likedOverviews[index].toString())
-                            ],
-                          )
-                        ],
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   );
                 }),
       ),
