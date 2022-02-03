cbe7ce6cc14e43ef6c4e2c7190b226256f5c9007
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2022-02-03 21:56:59 +0200

    .

---
+++ lib/data/liked.dart
@@ -0,0 +1,11 @@
+class Liked {
+  String poster = "";
+  String title = "";
+  String overview = "";
+
+  Liked(String poster, String title, String overview) {
+    poster = this.poster;
+    title = this.title;
+    overview = this.overview;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/services.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'package:tinder_for_movies/utils/imports.dart';
 
 void main() async {
@@ -15,7 +17,14 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
     return MaterialApp(
-      home: MovieHomeWidget(),
+      home: SplashScreenView(
+        navigateRoute: MovieHomeWidget(),
+        duration: 10000,
+        imageSize: 200,
+        imageSrc: "assets/logo.png",
+        backgroundColor: Colors.black,
+        pageRouteTransition: PageRouteTransition.CupertinoPageRoute,
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
