3177567e8cc0a5fc55bc48bdb39f4ae316a0516e
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2022-06-03 16:13:04 +0200

    .

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -21,7 +21,7 @@ class About extends StatelessWidget {
               height: 20,
             ),
             Text(
-                "Telly-Record is a tinder for movies type of application. You as the user are shown titles of movies/series which are currently playing/most popular/top rated/trending/upcoming as well as the descriptin and poster. You can decide to like them by swiping right and they will be added to your liked movies/series. If you dislike them from what has been given to you, you can swipe left to dislike. If you do not want to swipe, you can just use the like/dislike buttons. You are also able to revisit your library of liked series/movies when you now feel like watching them or updating the library by deleting some."),
+                "Telly-Record is a tinder for movies type of application. You as the user are shown titles of movies/series which are currently playing/most popular/top rated/trending/upcoming as well as the description and poster. You can decide to like them by swiping right and they will be added to your liked movies/series. If you dislike them from what has been given to you, you can swipe left to dislike. If you do not want to swipe, you can just use the like/dislike buttons. You are also able to revisit your library of liked series/movies when you now feel like watching them or updating the library by deleting some."),
             Row(
               children: [
                 Icon(MyFlutterApp.upcoming, size: 50),
